c1c8c20437005daa05effd4693892bf59a3c2e48
Md. Wahidul Haque <w.neon2000@gmail.com>
2024-05-11 15:23:03 +0600

    Adding 100 plus files at once

---
+++ config/db_config.php
@@ -0,0 +1,9 @@
+<?php
+	$db = mysqli_connect("localhost", "root", "", "wt_ecom");
+	if ( $db ){
+		// echo "Database Connected Successfully.";
+	}
+	else{
+		die("MySQLi Connection Failed.");
+	}
+?>
